8e73ea6c007e57af74abea32ed3d37b070fc0152
Michael Ferreira <93002921+Michael-Platini@users.noreply.github.com>
2022-08-09 22:17:06 -0300

    Aula dia 09/08/2022

--- React_Redux/frontend/frontend-ciclodepagamento/src/common/tab/tabActions.js
+++ React_Redux/frontend/frontend-ciclodepagamento/src/common/tab/tabActions.js
@@ -1,5 +1,5 @@
 export function selectTab(tabId) {
-    return{
+    return {
         type: 'TAB_SELECTED',
         payload: tabId
     }
@@ -7,8 +7,8 @@ export function selectTab(tabId) {
 
 export function showTabs(...tabIds) {
     const tabsToShow = {}
-    tabIds.forEach(e => tabsToShow [e] = true)
-    return{
+    tabIds.forEach(e => tabsToShow[e] = true)
+    return {
         type: 'TAB_SHOWED',
         payload: tabsToShow
     }

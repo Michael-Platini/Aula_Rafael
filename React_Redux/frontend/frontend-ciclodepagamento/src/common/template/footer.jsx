b6c57226def59883d6ae7883a54e804d82e6e370
Michael Ferreira <93002921+Michael-Platini@users.noreply.github.com>
2022-08-13 22:07:34 -0300

    Aula dia 13/08/2022

--- React_Redux/frontend/frontend-ciclodepagamento/src/common/template/footer.jsx
+++ React_Redux/frontend/frontend-ciclodepagamento/src/common/template/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
     <footer className='main-footer'> 
         <strong> 
-            Copyright &copy; 2017
+            Mefen &copy; 2022
             <a href='http://cod3r.com.br' target='_blank'> Cod3r</a>.
         </strong>
     </footer>

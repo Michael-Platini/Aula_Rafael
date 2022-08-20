452bc0be78d67af411ee744a7f17fbaae43f0094
Michael-Platini <michael.platini@live.com>
2022-08-19 22:16:09 -0300

    Aula dia 19/08/2022

---
+++ React_Redux/frontend/portifolio/src/pages/index.js
@@ -0,0 +1,25 @@
+import { FaArrowRight } from 'react-icons/fa'
+import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../../styles/indexStyle'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+   <Container>
+     <Content>
+      <Infos>
+        <Name>Olá, sou Michael Platini</Name>
+        <Function>FullStack Developer</Function>
+        <Intro>Cursando Analise e Desenvolvimento de Sistemas
+                  desenvolvimento Web (React, JS e outros)
+        </Intro>
+        <Link href="projects">
+          <LinkProjects>PROJETOS <FaArrowRight /></LinkProjects>
+        </Link>
+      </Infos>
+      <Logo>
+        <Img src="/images/logo.png" alt="logo" />
+      </Logo>
+     </Content>
+   </Container>
+  )
+}

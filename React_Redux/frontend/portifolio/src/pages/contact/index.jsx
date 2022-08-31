a7ff8f0a1de29f4e4830e69c59a660a469052e1a
Michael-Platini <michael.platini@live.com>
2022-08-30 22:48:12 -0300

    Aula dia 30/08/2022

--- React_Redux/frontend/portifolio/src/pages/contact/index.jsx
+++ React_Redux/frontend/portifolio/src/pages/contact/index.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
       <Content>
         <ItemContact 
           IconFa={FaLinkedin} 
-          LinkContact="https://www.linkedin.com/in/william-l-9b6625102" 
+          LinkContact="https://www.linkedin.com/in/michael-platini-8aa403228/" 
         />
         <ItemContact 
           IconFa={FaFacebook} 
